9c6fa14542327bf8b31ff44e4f866bf26794f252
randyfranzmeier <43656370+randyfranzmeier@users.noreply.github.com>
2024-10-12 22:30:30 -0500

    Init Commit.

---
+++ multi-threaded-apis/src/main/java/com/example/multithreadedapis/MultiThreadedApisApplication.java
@@ -0,0 +1,13 @@
+package com.example.multithreadedapis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiThreadedApisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiThreadedApisApplication.class, args);
+	}
+
+}
---
+++ multi-threaded-apis/src/test/java/com/example/multithreadedapis/MultiThreadedApisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.multithreadedapis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiThreadedApisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
