1a696871e3803f7392a5b4e16d1199febbbed2c0
randyfranzmeier <43656370+randyfranzmeier@users.noreply.github.com>
2024-10-27 14:02:55 -0500

    Adding 1 second delay to player service.

--- multi-threaded-apis/src/main/java/com/example/multithreadedapis/services/PlayerService.java
+++ multi-threaded-apis/src/main/java/com/example/multithreadedapis/services/PlayerService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class PlayerService {
@@ -15,15 +16,21 @@ public class PlayerService {
     }
 
     public static Player CreateAndGetRandomPlayer() {
-        List<Player> players = new ArrayList<Player>();
-        players.add(new Player(1, "Babe", "Ruth", 129, new Team("Yankees", Sport.Baseball)));
-        players.add(new Player(2, "Justin", "Jefferson", 25, new Team("Vikings", Sport.Football)));
-        players.add(new Player(3, "Michael", "Jordan", 61, new Team("Bulls", Sport.Basketball)));
-        players.add(new Player(4, "Wayne", "Gretzky", 63, new Team("Rangers", Sport.Hockey)));
-        players.add(new Player(5, "Christiano", "Ronaldo", 39, new Team("Al-Nassr", Sport.Soccer)));
+        try {
+            Thread.sleep(1000);
+            List<Player> players = new ArrayList<>();
+            players.add(new Player(1, "Babe", "Ruth", 129, new Team("Yankees", Sport.Baseball)));
+            players.add(new Player(2, "Justin", "Jefferson", 25, new Team("Vikings", Sport.Football)));
+            players.add(new Player(3, "Michael", "Jordan", 61, new Team("Bulls", Sport.Basketball)));
+            players.add(new Player(4, "Wayne", "Gretzky", 63, new Team("Rangers", Sport.Hockey)));
+            players.add(new Player(5, "Christiano", "Ronaldo", 39, new Team("Al-Nassr", Sport.Soccer)));
 
-        Random r = new Random();
-        int randomPlayer = r.nextInt(5 - 1 + 1) + 1;
-        return players.stream().filter(player -> player.getId() == randomPlayer).findFirst().get();
+            Random r = new Random();
+            int randomPlayer = r.nextInt(5 - 1 + 1) + 1;
+            return players.stream().filter(player -> player.getId() == randomPlayer).findFirst().get();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
