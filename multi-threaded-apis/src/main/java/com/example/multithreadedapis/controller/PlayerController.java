dad79987df6ecbb885760fa471ec09f6b3ad789d
randyfranzmeier <43656370+randyfranzmeier@users.noreply.github.com>
2024-10-27 14:01:01 -0500

    Adding logic to controller to compare the use of threads (threadpool) vs. a single thread.

--- multi-threaded-apis/src/main/java/com/example/multithreadedapis/controller/PlayerController.java
+++ multi-threaded-apis/src/main/java/com/example/multithreadedapis/controller/PlayerController.java
@@ -1,12 +1,24 @@
 package com.example.multithreadedapis.controller;
-
 import com.example.entity.Player;
+import com.example.entity.PlayerResponse;
 import com.example.multithreadedapis.services.PlayerService;
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.websocket.RemoteEndpoint;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
 
 @RestController
 @RequestMapping("/api")
@@ -19,9 +31,47 @@ public class PlayerController {
         this.playerService = playerService;
     }
 
-    @GetMapping("/player/nothreading")
-    public ResponseEntity<Player> getPlayerNoThreading() { //to get a path variable, use @PathVariable annotation
-        return ResponseEntity.ok(PlayerService.CreateAndGetRandomPlayer());
+    @GetMapping(value = "/player/nothreading/{numPlayers}", produces = {"application/json"})
+    public ResponseEntity<String> getPlayerNoThreading(@PathVariable int numPlayers) throws JsonProcessingException, InterruptedException { //to get a path variable, use @PathVariable annotation
+        long startTime = System.currentTimeMillis();
+        if (numPlayers < 1) {
+            throw new IllegalArgumentException("Number of players must be greater than 0");
+        }
+        //do everything one thread at a time (can't reuse threads)
+        for (int i = 0; i < numPlayers; i++) {
+            Thread thread = new Thread (() -> {
+                System.out.println(PlayerService.CreateAndGetRandomPlayer());
+            });
+            thread.start();
+            thread.join(); //wait for thread to finish
+        }
+        long endTime = System.currentTimeMillis();
+        PlayerResponse pr = new PlayerResponse(endTime-startTime, numPlayers);
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
+        return ResponseEntity.ok(mapper.writeValueAsString(pr));
+    }
+
+    @GetMapping("/player/thread-pool/{numPlayers}")
+    public ResponseEntity<String> getPlayerThreadPool(@PathVariable int numPlayers) {
+        try {
+            long startTime = System.currentTimeMillis();
+            ExecutorService executor = Executors.newFixedThreadPool(numPlayers);
+            for (int i = 0; i < numPlayers; i++) {
+                executor.submit(() -> {System.out.println(PlayerService.CreateAndGetRandomPlayer());});
+            }
+            executor.shutdown(); //wait for all the threads to finish, then close down the service
+            long endTime = System.currentTimeMillis();
+            PlayerResponse pr = new PlayerResponse(endTime-startTime, numPlayers);
+            ObjectMapper mapper = new ObjectMapper();
+            mapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
+            return ResponseEntity.ok(mapper.writeValueAsString(pr));
+
+        } catch(IllegalArgumentException e) {
+            throw new IllegalArgumentException("Number of players must be greater than 0");
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 
