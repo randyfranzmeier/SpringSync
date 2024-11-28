cd90db5574fc465ac28ea5e9f34435ad22c37c7a
randyfranzmeier <Randyfranzmeier@gmail.com>
2024-11-28 10:01:28 -0600

    Creating constants for each API endpoint.

---
+++ multi-threaded-apis/src/main/resources/static/concurrency/src/constants/endpoints.js
@@ -0,0 +1,5 @@
+export const API_URLS = {
+    CreatePlayersWithThreads: 'http://localhost:8080/api/player/thread-pool/',
+    CreatePlayerNoThreads: 'http://localhost:8080/api/player/nothreading/',
+    CounterTimer: 'http://localhost:8080/counter'
+}
