9375a9c4a3ad1c802d63a4343ca56477050bf763
randyfranzmeier <Randyfranzmeier@gmail.com>
2024-12-02 10:03:14 -0600

    Creating styles for components and whole page.

---
+++ multi-threaded-apis/src/main/resources/static/concurrency/src/components/create-players-no-threads/create-players-no-threads.styled.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const NoThreadsPlayerWrapper = styled.div`
+    text-align: center;
+`;
+
+export const ResponseContainer = styled.div
+`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    margin-top: 5%;
+`;
+
