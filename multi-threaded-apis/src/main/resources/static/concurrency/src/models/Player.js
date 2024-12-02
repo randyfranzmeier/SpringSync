f6b033d0c6e81a08909647ded41e95adcf70b669
randyfranzmeier <Randyfranzmeier@gmail.com>
2024-12-02 10:08:27 -0600

    Creating model

---
+++ multi-threaded-apis/src/main/resources/static/concurrency/src/models/Player.js
@@ -0,0 +1,10 @@
+export class Player {
+    time;
+    playersCreated;
+
+    constructor(playersCreated, time) {
+        this.playersCreated = playersCreated;
+        this.time = time;
+    }
+
+}
