296c1ed0d2a28fd6af8bcf6f30db207b28ba3bb4
randyfranzmeier <Randyfranzmeier@gmail.com>
2024-12-17 10:00:26 -0600

    Updating DTO for request body and specifying the production of json in post mapping.

--- multi-threaded-apis/src/main/java/com/example/multithreadedapis/controller/PlayerController.java
+++ multi-threaded-apis/src/main/java/com/example/multithreadedapis/controller/PlayerController.java
@@ -1,5 +1,5 @@
 package com.example.multithreadedapis.controller;
-import com.example.request.ThreadedPlayer;
+import com.example.request.ThreadedPlayerRequest;
 import com.example.response.PlayerResponse;
 import com.example.multithreadedapis.services.PlayerService;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
@@ -9,7 +9,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import java.util.concurrent.*;
 
 @RestController
 @RequestMapping("/api")
@@ -38,8 +37,8 @@ public class PlayerController {
         return ResponseEntity.ok(mapper.writeValueAsString(pr));
     }
 
-    @PostMapping("/player/threadpool")
-    public ResponseEntity<String> getPlayerThreadPool(@RequestBody ThreadedPlayer request) throws InterruptedException {
+    @PostMapping(value = "/player/threadpool", produces = {"application/json"})
+    public ResponseEntity<String> getPlayerThreadPool(@RequestBody ThreadedPlayerRequest request) throws InterruptedException {
         try {
             if (request.getNumThreads() < 1 || request.getNumThreads() > 20) {
                 return ResponseEntity.badRequest().body("");
