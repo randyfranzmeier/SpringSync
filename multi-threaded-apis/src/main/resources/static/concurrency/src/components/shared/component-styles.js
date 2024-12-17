6b26a71d24e386efb0b8c44983775720cbfd1d21
randyfranzmeier <Randyfranzmeier@gmail.com>
2024-12-16 18:31:36 -0600

    Adding styles to shared folder.

---
+++ multi-threaded-apis/src/main/resources/static/concurrency/src/components/shared/component-styles.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+
+export const ResponseContainer = styled.div
+    `
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    margin-top: 5%;
+`;
+
+export const TopMargin = styled.div
+    `
+    margin-top: 3%;
+`;
